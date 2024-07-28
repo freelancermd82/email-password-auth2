75a82ea74ece26ce30c016ad1d2d8453297d7cc5
Your Name <freelancermd82@gmail.com>
2024-07-28 07:01:18 +0600

    bootstrap reactBTS register page

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
 
+
 const Main = () => {
     return (
-        <div>
+        <div className='w-50 mx-auto mt-5'>
             <Header></Header>
             <Outlet></Outlet>
         </div>
